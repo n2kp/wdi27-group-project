e0956e20bdb62a8242e488e942b364b1724d6348
Nimesh Patel <n2kp@hotmail.com>
2017-07-06 13:59:07 +0100

    Title, Rainbow, Forms, vaildation styling added

--- src/js/constants/mapstyle.js
+++ src/js/constants/mapstyle.js
@@ -1,73 +1,73 @@
 angular
 .module('projectApp')
 .constant('mapStyles', [
-    {
-        "featureType": "landscape.natural",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#e0efef"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#1900ff"
-            },
-            {
-                "color": "#c0e8e8"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": 100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": 700
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#1792AE"
-            }
-        ]
-    }
+  {
+    'featureType': 'landscape.natural',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'visibility': 'on'
+      },
+      {
+        'color': '#48A15D'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'visibility': 'on'
+      },
+      {
+        'hue': '#1900ff'
+      },
+      {
+        'color': '#48A15D'
+      }
+    ]
+  },
+  {
+    'featureType': 'road',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'lightness': 100
+      },
+      {
+        'visibility': 'simplified'
+      }
+    ]
+  },
+  {
+    'featureType': 'road',
+    'elementType': 'labels',
+    'stylers': [
+      {
+        'visibility': 'off'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.line',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'visibility': 'on'
+      },
+      {
+        'lightness': 700
+      }
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'all',
+    'stylers': [
+      {
+        'color': '#1792AE'
+      }
+    ]
+  }
 ]);
