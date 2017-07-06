9ff56495dd3ab4c4845a1731f5103d08f21bbb1f
Angela Maugey <a.maugey@gmail.com>
2017-07-06 13:56:19 +0100

    Modified seeds file. Also users edit view.

--- db/seeds.js
+++ db/seeds.js
@@ -26,6 +26,21 @@ User
     username: 'Clare',
     password: 'password',
     passwordConfirmation: 'password'
+  },{
+    email: 'tina@ga',
+    username: 'TinaTurner',
+    password: 'password',
+    passwordConfirmation: 'password'
+  },{
+    email: 'barry@ga',
+    username: 'BarryWhite',
+    password: 'password',
+    passwordConfirmation: 'password'
+  },{
+    email: 'beyonce@ga',
+    username: 'Beyonce',
+    password: 'password',
+    passwordConfirmation: 'password'
   }])
 
   .then((users) => {
@@ -38,6 +53,7 @@ User
         image: 'https://s3-eu-west-1.amazonaws.com/wdi27-devbook/portfolio_001.png',
         projectUrl: 'https://jacekjeznach.com/',
         dateCreated: '2017-04-12',
+        tech: ['npm', 'HTML5', 'JavaScript', 'SASS', 'Jquery'] ,
         createdBy: users[0]
       },{
         title: 'Multi Projos',
@@ -45,6 +61,7 @@ User
         image: 'https://s3-eu-west-1.amazonaws.com/wdi27-devbook/portfolio_002.png',
         projectUrl: 'http://claudiocalautti.cc/',
         dateCreated: '2017-04-01',
+        tech: ['Mongodb', 'Gulp', 'Git', 'Bootstrap', 'SASS', 'HTML5', 'Nodejs'],
         createdBy: users[1]
       },{
         title: 'Saturn C',
@@ -52,6 +69,7 @@ User
         image: 'https://s3-eu-west-1.amazonaws.com/wdi27-devbook/portfolio_003.png',
         projectUrl: 'http://juanmartinez.co.uk/',
         dateCreated: '2015-06-21',
+        tech: ['ReactJS', 'JavaScript', 'Grunt', 'Git', 'Aws', 'Heroku', 'CSS3'],
         createdBy: users[2]
       },{
         title: 'Dark Grey',
@@ -59,6 +77,7 @@ User
         image: 'https://s3-eu-west-1.amazonaws.com/wdi27-devbook/portfolio_004.png',
         projectUrl: 'https://caferati.me/',
         dateCreated: '2014-05-31',
+        tech: ['npm', 'Git', 'Codepen', 'Jquery', 'JavaScript', 'HTML5', 'SASS'],
         createdBy: users[0]
       },{
         title: 'Colourful Icon',
@@ -66,6 +85,7 @@ User
         image: 'https://s3-eu-west-1.amazonaws.com/wdi27-devbook/portfolio_005.png',
         projectUrl: 'http://tsimenis.com/#mariettafafouti',
         dateCreated: '2017-05-12',
+        tech: ['Ruby', 'JavaScript', 'Heroku', 'Codepen', 'npm'],
         createdBy: users[1]
       },{
         title: 'Deep Blue Site',
@@ -73,6 +93,7 @@ User
         image: 'https://s3-eu-west-1.amazonaws.com/wdi27-devbook/portfolio_006.png',
         projectUrl: 'http://johnkavanagh.co.uk/',
         dateCreated: '2014-06-25',
+        tech: ['CSS3', 'Heroku', 'HTML5', 'SASS', 'Aws'],
         createdBy: users[2]
       },{
         title: 'Showing Off',
@@ -80,6 +101,7 @@ User
         image: 'https://s3-eu-west-1.amazonaws.com/wdi27-devbook/portfolio_007.png',
         projectUrl: 'https://roybarber.com/',
         dateCreated: '2015-04-24',
+        tech: ['Mongodb', 'Bootstrap', 'HTML5', 'SASS','Shell'],
         createdBy: users[0]
       },{
         title: 'Ford Store Game',
@@ -87,6 +109,7 @@ User
         image: 'https://s3-eu-west-1.amazonaws.com/wdi27-devbook/portfolio_008.png',
         projectUrl: 'http://joedarrington.com/',
         dateCreated: '2015-10-13',
+        tech: ['PHP', 'HTML5', 'Heroku', 'Gulp', 'Git'],
         createdBy: users[1]
       },{
         title: 'Lots of Projects',
@@ -94,7 +117,25 @@ User
         image: 'https://s3-eu-west-1.amazonaws.com/wdi27-devbook/portfolio_009.png',
         projectUrl: 'http://mikeybox.com/',
         dateCreated: '2017-02-03',
+        tech: ['Fedora', 'Git', 'Apache', 'Gulp', 'Aws'],
         createdBy: users[2]
+      },{
+        title: 'Guess Ru?',
+        description: 'A small game created in my first project at GA. Using JavaScript,  a board that hides an image, tiles are removed as a button is pressed. The score goes down as each tile is removed, the aim is to guess the drag queen image before there are no points left. Also a special bonus round at the end of the game!',
+        image: 'http://i.imgur.com/j5h5tAN.png',
+        projectUrl: 'https://sheltered-reef-48200.herokuapp.com/',
+        dateCreated: '2017-05-20',
+        tech: ['Git', 'Gulp', 'npm', 'Heroku', 'Jquery', 'HTML5', 'SASS', 'JavaScript'],
+        createdBy: users[1]
+      },{
+        title: 'Barry\'s Portfolio',
+        description: 'Portfolio that I created upon leaving the WDI course at GA. Check out the link and contact for more information',
+        image: 'http://i.imgur.com/3BvVoSJ.png',
+        projectUrl: 'https://unsplash.com/?photo=Nod1nQ0i5a0',
+        dateCreated: '2016-12-03',
+        tech: ['Ruby', 'HTML', 'SCSS', 'Bootstrap'],
+        createdBy: users[4]
+
       }]);
   })
   .then((projects) => console.log(`${projects.length} projects created`))
